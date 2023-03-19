2943aab70b533a8992b52d99c24468faa27f70fa
mdmasudrana5060 <ranam5060@gmail.com>
2023-03-19 12:48:48 +0600

    reverseArray solved

--- Array/reverseArray.js
+++ Array/reverseArray.js
@@ -1,12 +1,32 @@
 let array = [5, 10, 2, 3, 7, 9, 15];
-let length = array.length - 1;
-console.log(length);
-
-for (let i = 0, j = length; i < 3; i++, j--) {
-    let temp = array[i];
-    array[i] = array[j];
-    array[j] = temp
 
 
+function reverseArray(array) {
+    for (let i = 0, j = array.length - 1; i < Math.floor((array.length - 1) / 2); i++, j--) {
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp
+
+
+    }
+    return array;
 }
-console.log(array);
+
+
+console.log(reverseArray(array));
+// with while loop
+let array2 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+function reverseArray2(array) {
+    let a = 0;
+    let b = array.length - 1;
+    let temp;
+    while (a < b) {
+        temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+        a++;
+        b--;
+    }
+    return array;
+}
+console.log(reverseArray2(array2));
